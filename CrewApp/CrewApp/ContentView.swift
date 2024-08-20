f3e5d8922f68abcc87a25285b89723757f753c44
harikrishna-22 <harikrishna.s2000@gmail.com>
2024-08-20 08:33:26 -0700

    init

---
+++ CrewApp/CrewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrewApp
+//
+//  Created by Crew on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrewApp/CrewApp/CrewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewAppApp.swift
+//  CrewApp
+//
+//  Created by Crew on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CrewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
